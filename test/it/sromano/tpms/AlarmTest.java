9febd6014219b9257e2d08a34b37efe460affc58
sromano87 <siromano@unisa.it>
2023-04-03 14:56:23 +0200

    Added package and test class

---
+++ test/it/sromano/tpms/AlarmTest.java
@@ -0,0 +1,14 @@
+package it.sromano.tpms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlarmTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
