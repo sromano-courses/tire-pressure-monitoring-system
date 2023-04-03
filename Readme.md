b67114072882b9b3cbfe0f3f1cd5566a78d46113
sromano87 <siromano@unisa.it>
2023-04-03 14:52:23 +0200

    Initialized

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# TirePressureMonitoringSystem
+
+Write the unit tests for the Alarm class, refactor the code as much as you need to make the class testable.
+
+The Alarm class is designed to monitor tire pressure and set an alarm if the pressure falls outside of the expected range. The Sensor class provided for the exercise simulates the behaviour of a real tire sensor, providing random but realistic values.
---
+++ src/it/sromano/tpms/Alarm.java
@@ -0,0 +1,22 @@
+package it.sromano.tpms;
+
+public class Alarm {
+	private final double LowPressureThreshold = 17;
+	private final double HighPressureThreshold = 21;
+
+	Sensor sensor = new Sensor();
+
+	boolean alarmOn = false;
+
+	public void check() {
+		double psiPressureValue = sensor.popNextPressurePsiValue();
+
+		if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue) {
+			alarmOn = true;
+		}
+	}
+
+	public boolean isAlarmOn() {
+		return alarmOn;
+	}
+}
